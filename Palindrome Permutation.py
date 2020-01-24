6fd0806258aaad3b296cf4d6ad0e5bfb0729894d
antonyohoward <52841359+antonyohoward@users.noreply.github.com>
2020-01-24 17:56:34 -0500

    Create Palindrome Permutation.py
    Takes a string and checks if it is a permutation of a palindrome. Palindrome does not have to be limited to dictionary words

---
+++ Palindrome Permutation.py
@@ -0,0 +1,22 @@
+def isPermofPali(string):
+	true_length = string.replace(" ", "").lower()
+	if (len(true_length)%2) == 0:
+		return False
+	sorted_true = sorted(true_length)
+	odd = 0
+	for i in range(0, len(sorted_true)):
+		count=sorted_true.count(sorted_true[i])	
+		if (count%2) == 0:
+			continue
+		else:
+			odd += 1
+	if odd != 1:
+		return False
+	else:
+		return True
+		
+str = raw_input('')
+if isPermofPali(str):
+	print 'Yes'
+else:
+	print 'No'
